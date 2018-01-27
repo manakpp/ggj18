181b12de7761466e9782b0ac282f839102814259
Mana Khamphanpheng <mana.kpp@gmail.com>
2018-01-27 18:35:53 +1300

    some input and character control... need an xbox controller :S

---
+++ ggj18/Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour 
+{
+	private GamepadInput m_gamepadInput;
+
+	public GamepadInput Input { get { return m_gamepadInput; } }
+
+	private void Awake()
+	{
+		GameContext.Add (this);	
+
+		m_gamepadInput = GetComponent<GamepadInput>();
+	}
+
+	public GamepadDevice GetDevice(int index)
+	{
+		if (m_gamepadInput.gamepads == null ||
+		   index <= m_gamepadInput.gamepads.Count)
+			return null;
+
+		return m_gamepadInput.gamepads [index];
+	}
+}
---
+++ ggj18/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour 
+{
+	private PlayerCharacterController m_controller;
+	private int m_playerIndex = 0;
+
+	public void InitialisePlayer(PlayerCharacterController controller, int index)
+	{
+		m_controller = controller;
+		m_playerIndex = 0;
+	}
+
+	private void Update()
+	{
+		GamepadDevice device = GameContext.Get<InputManager> ().GetDevice(m_playerIndex);
+		if (device == null)
+			return;
+
+		if(device.GetButtonUp(GamepadButton.Action1))
+		{
+			// Change thought
+		}
+		else if(device.GetButtonUp(GamepadButton.Action2))
+		{
+			// Change thought
+		}
+		else if(device.GetButtonUp(GamepadButton.Action3))
+		{
+			// Change thought
+		}
+		else if(device.GetButtonUp(GamepadButton.Action4))
+		{
+			// Change thought
+		}
+
+		float moveX = device.GetAxis (GamepadAxis.LeftStickX);
+		float moveY = device.GetAxis (GamepadAxis.LeftStickY);
+		float deadzone = 0.01f;
+		if (moveX > deadzone || moveX < deadzone) 
+		{
+			// Move horizontal
+		}
+		if (moveY > deadzone || moveY < deadzone) 
+		{
+			// Move vertical
+		}
+	}
+}
